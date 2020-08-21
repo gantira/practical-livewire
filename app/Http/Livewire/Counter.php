8282e9c980e5cde2d3d72c99845f7a68b222b633
Angga Gantira <angga.gantira@gmail.com>
2020-08-21 22:15:55 +0700

    1 Komponen pertama kita

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $counter = 1;
+
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+
+    public function increase()
+    {
+        $this->counter++;
+    }
+
+    public function decrease()
+    {
+        if ($this->counter < 1) $this->counter = 1;
+
+        $this->counter--;
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,12 @@
+<div class="card">
+    <div class="card-header">Hello Livewire</div>
+
+    <div class="card-body">
+        <div class="form-inline">
+            <button class="btn btn-primary" wire:click.prevent="decrease">-</button>
+            <span class="mx-4">{{ $counter }}</span>
+            <button class="btn btn-primary" wire:click.prevent="increase">+</button>
+        </div>
+
+    </div>
+</div>
