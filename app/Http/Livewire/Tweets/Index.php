e7bbe48ac1c04a6bce74c8e51bb3a97bbca0c3aa
Angga Gantira <angga.gantira@gmail.com>
2020-08-22 10:51:06 +0700

    3 Paginasi atau load more ?

--- app/Http/Livewire/Tweets/Index.php
+++ app/Http/Livewire/Tweets/Index.php
@@ -4,12 +4,23 @@ namespace App\Http\Livewire\Tweets;
 
 use App\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    public $perPage = 3;
+
+    public function loadMore()
+    {
+        $this->perPage += 3;
+    }
+
     public function render()
     {
-        $tweets = Tweet::latest()->get();
+        $tweets = Tweet::latest()->paginate($this->perPage);
+
         return view('livewire.tweets.index', compact('tweets'));
     }
 }
--- resources/views/livewire/tweets/index.blade.php
+++ resources/views/livewire/tweets/index.blade.php
@@ -3,8 +3,14 @@
         <div class="card-header">Timeline</div>
         <div class="card-body">
             @foreach ($tweets as $tweet)
-            <livewire:tweets.single :key="$tweet->id" :tweet="$tweet">
+                <livewire:tweets.single :key="$tweet->id" :tweet="$tweet">
             @endforeach
+
+            {{-- {{ $tweets->links() }} --}}
+
+            @if ($tweets->hasMorePages())
+                <button class="btn btn-primary" type="button" wire:click.prevent="loadMore">Load More!</button>
+            @endif
         </div>
     </div>
 </div>
